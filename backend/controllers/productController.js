ccbf564935284c3372e73639847970374ae4f7f5
Kinzamalik <kiinza.malik@gmail.com>
2022-06-27 00:07:25 +0500

    error handler

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../Models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
 
 // create Product - admin
 exports.createProduct = async(req,res,next)=>{
@@ -28,12 +29,9 @@ exports.getProductDetails = async(req,res,next)=>{
     const product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"Product not found"
-
-        })
+        return next(new  ErrorHandler("Product not found", 404));    
     }
+    
     res.status(200).json({
       success: true,
       product
